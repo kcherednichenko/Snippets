a54e5206b6840fb9bbce03ab2e8b0378418e9030
Ekaterina <cherednichenko_kate@mail.ru>
2023-07-13 17:13:59 +0300

    update snippets view

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,8 @@
 from django.http import Http404
 from django.shortcuts import render, redirect
 
+from MainApp.models import Snippet
+
 
 def index_page(request):
     context = {'pagename': 'PythonBin'}
@@ -13,5 +15,12 @@ def add_snippet_page(request):
 
 
 def snippets_page(request):
-    context = {'pagename': 'Просмотр сниппетов'}
+    snippets = Snippet.objects.all()
+    snippets_cnt = Snippet.objects.count()
+
+    context = {
+        'snippets': snippets,
+        'snippets_cnt': snippets_cnt
+    }
+
     return render(request, 'pages/view_snippets.html', context)
