2eb88666a9ff5bd34f81990a70f3d54ddae02c78
Ekaterina <cherednichenko_kate@mail.ru>
2023-07-17 17:05:33 +0300

    add django admin

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -1,9 +1,11 @@
+from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from MainApp import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index_page, name='home'),
     path('snippets/add', views.add_snippet_page, name='add_snippet_page'),
     path('snippets/list', views.snippets_page, name='snippets_list'),
@@ -13,5 +15,5 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     path('snippets/my', views.my_snippets, name='my-snippets'),
-    path('auth/register', views.create_user, name='register')
+    path('auth/register', views.create_user, name='register'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
